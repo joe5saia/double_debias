817e56c35aa0bac5143e2e028bd05b18a9353e67
Joe Saia <joe5saia@gmail.com>
2020-11-30 23:07:04 +0000

    make tests run if main

--- double_debias/tests/code_run_test.py
+++ double_debias/tests/code_run_test.py
@@ -1,19 +1,10 @@
 from unittest import TestCase
+import unittest
 from math import isclose
 import numpy as np
-import double_debias as ddd
+import double_debias as dd
 from sklearn.ensemble import GradientBoostingRegressor
 
-
-ob = dd.double_debias(y=np.array([i for i in range(0,10)]), 
-                   D= np.array([i//2 for i in range(0,10)]),
-                   z=np.array([[i**2 for i in range(0,10)], [i**3 for i in range(0,10)]]).transpose(),
-                   y_method= GradientBoostingRegressor(n_estimators=1000),
-                   D_method= GradientBoostingRegressor(n_estimators=1000),
-                   n_folds=3)
-
-
-ob.est_theta()
 
 class TestDD(TestCase):
     def test_run(self):
@@ -24,4 +15,7 @@ class TestDD(TestCase):
                    D_method= GradientBoostingRegressor(n_estimators=1000),
                    n_folds=3)
         theta = ob.est_theta()           
-        self.assertTrue(isclose(theta, -0.6, abs_tol=1e-1) )
+        self.assertAlmostEqual(theta, -0.6, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
