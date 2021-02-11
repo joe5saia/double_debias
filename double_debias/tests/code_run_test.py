e443fcdd42fa00fcb563a4a99c6c5d47368d1f86
Joe Saia <joe5saia@gmail.com>
2021-02-11 09:31:35 -0500

    Add more represenative test cases

--- double_debias/tests/code_run_test.py
+++ double_debias/tests/code_run_test.py
@@ -1,24 +1,68 @@
-from unittest import TestCase
-import unittest
+import pytest
 import numpy as np
-import double_debias as dd
-from sklearn.ensemble import GradientBoostingRegressor
+from double_debias import double_debias
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
 
 
-class TestDD(TestCase):
-    def test_run(self):
-        ob = dd.double_debias(y=np.array([i for i in range(0, 10)]),
-                              D=np.array([i//2 for i in range(0, 10)]),
-                              z=np.array(
-                                  [[i**2 for i in range(0, 10)], [i**3 for i in range(0, 10)]]).transpose(),
-                              y_method=GradientBoostingRegressor(
-                                  n_estimators=1000),
-                              D_method=GradientBoostingRegressor(
-                                  n_estimators=1000),
-                              n_folds=3)
-        theta = ob.est_theta()
-        self.assertAlmostEqual(theta, -0.6, places=2)
+def basetest(nregressors):
+    # Linear regression with a bunch of meaningless regressors. Y~D + epsilon in the base model
+    X, y, w = make_regression(n_samples=10000, n_informative=nregressors, n_features=10, coef=True, random_state=1, bias=3.5, shuffle=False, noise=0.001)
+    dd = double_debias(y=y, D=X[:, :nregressors], z=X[:, nregressors:], y_method=LinearRegression(), D_method=LinearRegression(), n_folds=3)
+    theta = dd.est_theta()
+    np.testing.assert_allclose(w[:nregressors], theta, rtol=0.1)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_1d():
+    # Test for 2-D D matrix with a single column
+    basetest(1)
+
+
+def test_2d():
+    # Test for 2-D D matrix with 2 columns
+    basetest(2)
+
+
+def test_confunders():
+    # Linear regression with confunders. Y ~ D + z + epsilon and E[DZ] != 0
+    # Verify that this works when there are confunders
+    z, D, w = make_regression(n_samples=100000, n_informative=8, n_features=10, coef=True, random_state=1, bias=3.5, shuffle=False, noise=10)
+    y = D + z.dot(np.random.rand(z.shape[1]))
+    D = D.reshape(-1, 1)
+    dd = double_debias(y=y, D=D, z=z, y_method=LinearRegression(), D_method=LinearRegression(), n_folds=3)
+    theta = dd.est_theta()
+    np.testing.assert_allclose(1, theta, rtol=0.1)
+
+
+def test_nonlinear(nregressors=2):
+    # Nonlinear regression with a bunch of meaningless regressors. Y ~ D + epsilon in the base model
+    # Tests that this works for 2-D D matrix with a single column
+    X, y, w = make_regression(n_samples=3000, n_informative=nregressors, n_features=10, coef=True, random_state=1, bias=3.5, shuffle=False, noise=0.001)
+    dd = double_debias(y=y, D=X[:, :nregressors], z=X[:, nregressors:], y_method=RandomForestRegressor(), D_method=RandomForestRegressor(), n_folds=3)
+    theta = dd.est_theta()
+    np.testing.assert_allclose(w[:nregressors], theta, rtol=0.1)
+
+
+def test_nonlinear2():
+    # Nonlinear regression with a bunch of meaningless regressors. Y ~ D + z+ epsilon in the base model
+    # Tests that this works for 2-D D matrix with 1 column
+    z, D, w = make_regression(n_samples=100000, n_informative=2, n_features=10, coef=True, random_state=1, bias=3.5, shuffle=False, noise=10)
+    y = D + (z[:, :2]**2).dot(np.random.rand(2))
+    D = D.reshape(-1, 1)
+    dd = double_debias(y=y, D=D, z=z, y_method=RandomForestRegressor(), D_method=RandomForestRegressor(), n_folds=3)
+    theta = dd.est_theta()
+    np.testing.assert_allclose(1, theta, rtol=0.1)
+
+
+def test_nonlinear3():
+    # Nonlinear regression with a bunch of meaningless regressors. Y ~ D + z+ epsilon in the base model
+    # Tests that this works for 2-D D matrix with 2 columns
+    # Non optimized model so results are only approximently correct.
+    z, D1, w = make_regression(n_samples=100000, n_informative=2, n_features=10, coef=True, random_state=1, bias=3.5, shuffle=False, noise=30)
+    z, D2, w = make_regression(n_samples=100000, n_informative=2, n_features=10, coef=True, random_state=5, bias=3.5, shuffle=False, noise=30)
+    D = np.stack([D1, D2], axis=1)
+    y = D.dot(np.array([-20.0, 40.0])) + (z[:, :2]**2).dot(np.random.rand(2))
+    dd = double_debias(y=y, D=D, z=z, y_method=RandomForestRegressor(), D_method=RandomForestRegressor(), n_folds=3)
+    theta = dd.est_theta()
+    np.testing.assert_allclose([-20.0, 40.0], theta, atol=5)
